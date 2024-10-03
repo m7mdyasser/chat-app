887bd7a241e2d65692b6c6edc22cb679088b7aa4
yasser <my5033585@gmail.com>
2024-10-03 19:45:08 +0300

    chat-app v 1.0.1

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { HashRouter } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { AuthContextProvider } from './Context/AuthContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter>
+    <Router>
       <AuthContextProvider>
         <App />
       </AuthContextProvider>
-    </HashRouter>
+    </Router>
   </React.StrictMode>,
 )
